bfc018647e1b04f5a29673be3e0afaad5532e63f
jbi9 <70661646+jbi9@users.noreply.github.com>
2022-04-14 11:45:44 -0400

    Add setup-small.sql

---
+++ Submission/setup-small.sql
@@ -0,0 +1,102 @@
+CREATE TABLE Demographic(
+	sex VARCHAR(10),
+	PRIMARY KEY (sex)
+);
+
+CREATE TABLE Year(
+	year INT,
+	PRIMARY KEY (year)
+);
+
+CREATE TABLE Country(
+	countryCode VARCHAR(3), 
+	countryName VARCHAR(200), 
+	continent VARCHAR(200), 
+	paidVacDays INT, 
+	paidHolidy INT, 
+	paidLeaveTotal INT
+	PRIMARY KEY (countryCode)
+);
+
+CREATE TABLE AnnualCountryStats(
+	countryCode VARCHAR(3),
+	year INT,
+	pctUsingInternet FLOAT,
+	GDPperCap FLOAT,
+	population INT,
+	fertRate FLOAT,
+	lifeSatisfaction FLOAT,
+	PRIMARY KEY (countryCode, year),
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (year) REFERENCES Year(year) ON DELETE CASCADE ON UPDATE CASCADE,
+	CHECK (pctUsingInternet >= 0 AND pctUsingInternet <= 100) 
+);
+
+
+CREATE TABLE AnnualDemographicStats(
+	countryCode VARCHAR(3),
+	year INT,
+	sex VARCHAR(10),
+	laborForcePartipation FLOAT
+	pctAdvancedEdu FLOAT,
+	pctBasicEdu FLOAT,
+	lifeExpect INT,
+	fertRate FLOAT,
+	literacyRate FLOAT,
+	PRIMARY KEY (countryCode, year, sex),
+	FOREIGN KEY (year) REFERENCES Year(year) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (sex) REFERENCES Demographic(sex),
+	CHECK (pctAdvancedEdu >= 0 AND pctAdvancedEdu <= 100),
+	CHECK (pctBasicEdu >= 0 AND pctBasicEdu <= 100),
+	CHECK (literacyRate >= 0 AND literacyRate <= 100)
+);
+
+
+CREATE TABLE WorksIn(
+	sectorID, 
+	countryCode, 
+	year, 
+	sex, 
+	monthlyEarnings,
+	PRIMARY KEY (sectorID, countryCode, year, sex),
+	FOREIGN KEY (year) REFERENCES Year(year) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (sex) REFERENCES Demographic(sex)
+);
+
+LOAD DATA LOCAL INFILE 'AnnualCountryStats-small.txt'
+INTO TABLE AnnualCountryStats
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'AnnualDemographicStats-small.txt'
+INTO TABLE AnnualDemographicStats
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'Year-small.txt'
+INTO TABLE Year
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'Country-small.txt'
+INTO TABLE Year
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'Demographics-small.txt'
+INTO TABLE Demographic
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'WorksIn-small.txt'
+INTO TABLE WorksIn
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
