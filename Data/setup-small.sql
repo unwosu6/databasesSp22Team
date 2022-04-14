0f85bd76b3118ea5ca2623594f75c4ae4d2ca6cb
Udochukwu Nwosu <unwosu6@jhu.edu>
2022-04-14 00:52:01 -0400

    added to sql file

--- Data/setup-small.sql
+++ Data/setup-small.sql
@@ -12,7 +12,27 @@ CREATE TABLE AnnualCountryStats(
 	PRIMARY KEY (countryCode, year)
 );
 
+CREATE TABLE AnnualDemoStats(
+	countryCode VARCHAR(5),
+	year INT,
+	sex VARCHAR(10),
+	laborForcePartipation FLOAT
+	pctAdvancedEdu FLOAT,
+	pctBasicEdu FLOAT,
+	lifeExpect INT,
+	fertRate FLOAT,
+	literacyRate FLOAT,
+	PRIMARY KEY (countryCode, year, sex)
+);
+
 LOAD DATA LOCAL INFILE 'AnnualCountryStats-small.txt'
 INTO TABLE AnnualCountryStats
 FIELDS TERMINATED BY '\t'
-LINES TERMINATED BY '\n';
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'AnnualDemoStats-small.txt'
+INTO TABLE AnnualCountryStats
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
