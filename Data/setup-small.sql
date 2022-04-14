09f17d17da3580f0f71cec09d6c8bf6530ebf144
jbi9 <70661646+jbi9@users.noreply.github.com>
2022-04-14 09:47:29 -0400

    Create table for all except Country done, foreign keys and primary keys added

--- Data/setup-small.sql
+++ Data/setup-small.sql
@@ -1,5 +1,18 @@
 DROP TABLE IF EXISTS AnnualCountryStats;
 
+CREATE TABLE Demographic(
+	sex VARCHAR(10),
+	PRIMARY KEY (sex)
+);
+
+CREATE TABLE Year(
+	year INT,
+	PRIMARY KEY (year)
+);
+
+
+--CREATE TABLE Country
+
 CREATE TABLE AnnualCountryStats(
 	countryCode VARCHAR(5),
 	year INT,
@@ -8,10 +21,13 @@ CREATE TABLE AnnualCountryStats(
 	population INT,
 	fertRate FLOAT,
 	lifeSatisfaction FLOAT,
-	PRIMARY KEY (countryCode, year)
+	PRIMARY KEY (countryCode, year),
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (year) REFERENCES Year(year),
+	CHECK (pctUsingInternet >= 0 AND pctUsingInternet <= 100) 
 );
 
-CREATE TABLE AnnualDemoStats(
+CREATE TABLE AnnualDemographicStats(
 	countryCode VARCHAR(5),
 	year INT,
 	sex VARCHAR(10),
@@ -21,7 +37,26 @@ CREATE TABLE AnnualDemoStats(
 	lifeExpect INT,
 	fertRate FLOAT,
 	literacyRate FLOAT,
-	PRIMARY KEY (countryCode, year, sex)
+	PRIMARY KEY (countryCode, year, sex),
+	FOREIGN KEY (year) REFERENCES Year(year),
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (sex) REFERENCES Demographic(sex),
+	CHECK (pctAdvancedEdu >= 0 AND pctAdvancedEdu <= 100),
+	CHECK (pctBasicEdu >= 0 AND pctBasicEdu <= 100),
+	CHECK (literacyRate >= 0 AND literacyRate <= 100)
+);
+
+
+CREATE TABLE WorksIn(
+	sectorID, 
+	countryCode, 
+	year, 
+	sex, 
+	monthlyEarnings,
+	PRIMARY KEY (sectorID, countryCode, year, sex),
+	FOREIGN KEY (year) REFERENCES Year(year),
+	FOREIGN KEY (countryCode) REFERENCES Country(countryCode),
+	FOREIGN KEY (sex) REFERENCES Demographic(sex)
 );
 
 LOAD DATA LOCAL INFILE 'AnnualCountryStats-small.txt'
