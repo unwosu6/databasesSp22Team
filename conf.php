ee3794ac1418f08f27c86c545f5e1897c15ad898
Udochukwu Nwosu <unwosu6@jhu.edu>
2022-05-04 15:10:14 -0400

    added html file for phase E

---
+++ conf.php
@@ -0,0 +1,8 @@
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "dbase.cs.jhu.edu"; // database host
+	$dbuser = "22sp_unwosu6";    // username on db host: replace 22sp_jhopkin1 with yours
+	$dbpass = "pLCySm5gCk";            // password on db host: replace ***** with yours
+	$dbname = "22sp_unwosu6_db"; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
---
+++ open.php
@@ -0,0 +1,15 @@
+<?php
+
+	// collect login variable values
+	include 'conf.php';  //make sure you've put your credentials in conf.php
+
+	// attempt to create a connection to db
+	$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+	// report whether failure or success
+	if ($conn->connect_errno) {
+	   echo("Connect failed: \n".$conn->connect_error);
+	   exit();
+	}
+
+?>
